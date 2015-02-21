81328c5d80121948d46028d9c451282a9c76992d
Dan Bucci <dlbucci@andrew.cmu.edu>
2015-02-20 23:09:33 -0500

    Completed Competitive Anal

--- competitive.tpl.php
+++ competitive.tpl.php
@@ -1,15 +1,22 @@
-<img src="images/compAnalysis1.jpg" alt="cmuCobots" width="127" height="108" style="float:right; clear:right;">
-<img src="images/compAnalysis2.png" alt="cmuCobots" width="147" height="96" style="float:right; clear:right;">
+<style type="text/css">
+img.comp-anal-image {
+  float:right;
+}
+</style>
 <p>There a several examples of autonomous robots and computer-vision assistance:</p>
 <ul>
-  <li>Cars (parallel parking, lane departure, self-driving)</li>
   <li>
-    ViSP (software for computer vision, image processing, visual servoing)
+    Cars (<a href="http://en.wikipedia.org/wiki/Automatic_parking" target="_blank">parallel parking</a>, <a href="http://en.wikipedia.org/wiki/Lane_departure_warning_system" target="_blank">lane departure</a>, <a href="http://en.wikipedia.org/wiki/Google_driverless_car" target="_blank">self-driving</a>) - Cars have begun to adopt various technologies to assist with driving, ranging from lane-departure warnings to autonomous, self-driving cars.  Our robots will obviously not be anywhere near this advanced, but they should be able to detect local danger and be able to prevent themselves from going over cliffs and hitting objects.
   </li>
   <li>
-    Roomba
+    <a href="http://www.irisa.fr/lagadic/visp/visp.html" target="_blank">ViSP</a> - ViSP is software that uses computer vision to control robotic systems.  Likewise, we plan to use images and image processing to plan and control our robots to achieve efficent maximum terrain coverage.
   </li>
   <li>
-    CMU CoBots
+    <a href="http://www.irobot.com/For-the-Home/Vacuum-Cleaning/Roomba.aspx" target="_blank">Roombas</a> - The Roomba was the inspiration for the project; it moves about enclosed environments, attempting to cover as much area as possible in the shortest time possible.  Our goal is to have our robots be more efficient in their coverage than the standard Roomba, with better planning and group coordination provided by the central hub.
+    <img src="images/compAnalysis1.jpg" class="comp-anal-image" alt="iRobot Roombas" width="127" />
+  </li>
+  <li>
+    <a href="http://www.cs.cmu.edu/~coral/projects/cobot/" target="_blank">CMU CoBots</a> - The CMU CoBots are advanced autonomous robots that can move about Gates-Hillman Center, asking students to move out of their way when necessary.  While we aim to achieve the same end product, our robots will come in a much smaller form factor, at the cost of cameras needing to be installed in the ceiling.
+    <img src="images/compAnalysis2.png" class="comp-anal-image" alt="CMU Cobots" width="147" />
   </li>
 </ul>
