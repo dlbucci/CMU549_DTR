cd4362e0fba24d00c4ae71b4f73e07f434059829
Dan Bucci <dlbucci@andrew.cmu.edu>
2015-04-15 14:49:12 -0400

    Fixes

--- index.tpl.php
+++ index.tpl.php
@@ -1,11 +1,12 @@
 <h3>Robot Sensor Test</h3>
 
 <div>
-  <video src="videos/sensor-test.mp4">Dude, upgrade your browser already.</video>
+  <video src="videos/sensor-test.mp4" height="480" controls>Dude, upgrade your browser already.</video>
 </div>
 
 <h3>Raspberry Pi Camera Test</h3>
 
 <div>
-  <video src="videos/camera-test.mp4">Come on, man.  It's 2015.</video>
+  <video src="videos/camera-test.mp4" height="480" controls class="rotate-left">Come on, man.  It's 2015.</video>
 </div>
+<div>Thank Harsha for the dumb orientation</div>
