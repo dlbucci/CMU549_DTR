d6c97a344b154ace5868571635dd4703e0c4d8db
Dan Bucci <dlbucci@andrew.cmu.edu>
2015-04-24 16:54:40 -0400

    Made it Uglier

--- base.php
+++ base.php
@@ -7,34 +7,39 @@
     <link rel="stylesheet" href="assets/style.css"/>
   </head>
   <body>
-    <h1 class="project-title">Distributed Tabletop Robots</h1>
-    
-    <h4 class="team-name">
-      18-549 Team 11:
-      <ul class="project-authors">
-        <li>Ibukun Adeleye (iia)</li>
-        <li>Dale Best (dbest)</li>
-        <li>Daniel Bucci (dlbucci)</li>
-        <li>Sriharsha Vasadi (svasadi)</li>
+    <header class="box">
+      <h1 class="project-title"><a href="index.php">Distributed Tabletop Robots</a></h1>
+      
+      <h4 class="team-name">
+        18-549 Team 11:
+        <ul class="project-authors">
+          <li>Ibukun Adeleye (iia)</li>
+          <li>Dale Best (dbest)</li>
+          <li>Daniel Bucci (dlbucci)</li>
+          <li>Sriharsha Vasadi (svasadi)</li>
+        </ul>
+      </h4>
+    </header>
+
+    <nav class="box">
+      <ul class="nav">
+        <li><a href="concept.php">Concept</a></li>
+        <li><a href="competitive.php">Competitive Analysis</a></li>
+        <li><a href="motivation.php">Motivation</a></li>
+        <li><a href="requirements.php">Requirements</a></li>
+        <li><a href="components.php">Components</a></li>
+        <li><a href="architecture.php">Architecture</a></li>
+        <li><a href="useCases.php">Use Cases</a></li>
+        <li><a href="interactions.php">Interaction</a></li>
       </ul>
-    </h3>
+    </nav>
 
-    <ul class="nav">
-      <li><a href="concept.php">Concept</a></li>
-      <li><a href="competitive.php">Competitive Analysis</a></li>
-      <li><a href="motivation.php">Motivation</a></li>
-      <li><a href="requirements.php">Requirements</a></li>
-      <li><a href="components.php">Components</a></li>
-      <li><a href="architecture.php">Architecture</a></li>
-      <li><a href="useCases.php">Use Cases</a></li>
-      <li><a href="interactions.php">Interaction</a></li>
-    </ul>
+    <h2 class="box page-title"><?= $title ?></h2>
 
-    <div id="mainContent" class="content">
-      <h2 class="page-title"><?= $title ?></h1>
+    <div id="main" class="box">
       <? include $content ?>
     </div>
 
-    <div id="footer" class="content">Created by 18-549 Team 11 for the Spring 2015 Semester.</div>
+    <footer class="box">Created by Team 11 for the Spring 2015 Semester of 18-549.</footer>
   </body>
 </html>
--- index.tpl.php
+++ index.tpl.php
@@ -1,17 +1,25 @@
-<h3>Robot Sensor Test</h3>
+<div class="video box">
+  <h3>Round Trip Test</h3>
 
-<div>
-  <video src="videos/sensor-test.mp4" height="480" controls>Dude, upgrade your browser already.</video>
+  <p>This was filmed on April 24th, 2015.  It demonstrates the bare-minimum of the interactions we hope to achieve.</p>
+
+  <div>
+    <video src="videos/roundtrip-test.mp4" height="480" controls class="rotate-left">Get it together, dawg.</video>
+  </div>
 </div>
 
-<h3>Raspberry Pi Camera Test</h3>
+<div class="video box">
+  <h3>Raspberry Pi Camera Test</h3>
 
-<div>
-  <video src="videos/camera-test.mp4" height="480" controls class="rotate-left">Come on, man.  It's 2015.</video>
+  <div>
+    <video src="videos/camera-test.mp4" height="480" controls class="rotate-left">Come on, man.  It's 2015.</video>
+  </div>
 </div>
 
-<h3>Round Trip Test</h3>
+<div class="video box">
+  <h3>Robot Sensor Test</h3>
 
-<div>
-  <video src="videos/roundtrip-test.mp4" height="480" controls class="rotate-left">Get it together, dawg.</video>
+  <div>
+    <video src="videos/sensor-test.mp4" height="480" controls>Dude, upgrade your browser already.</video>
+  </div>
 </div>
