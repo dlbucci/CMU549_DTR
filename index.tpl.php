c6705a03d4e0c858494ab122c4cb76faf8482e9a
Daniel Bucci <dlbucci@superftc.com>
2015-04-24 00:23:28 -0400

    Added Round Trip Video

--- index.tpl.php
+++ index.tpl.php
@@ -9,3 +9,9 @@
 <div>
   <video src="videos/camera-test.mp4" height="480" controls class="rotate-left">Come on, man.  It's 2015.</video>
 </div>
+
+<h3>Round Trip Test</h3>
+
+<div>
+  <video src="videos/roundtrip-test.mp4" height="480" controls class="rotate-left">Get it together, dawg.</video>
+</div>
