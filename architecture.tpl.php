67cfb2fb5e8f57b7f32645819a6ee91725b40441
harsha2512 <vasadi.sriharsha@gmail.com>
2015-04-25 17:03:43 -0400

    Update architecture.tpl.php

--- architecture.tpl.php
+++ architecture.tpl.php
@@ -1,1 +1,11 @@
-(TODO)
+<h3>Schematic</h3>
+
+<ul>
+  <li>
+    <p>Schematic</p>
+    <figure>
+      <img src="images/camera.png" alt="camera" width="180" height="108" align="top" />
+      <figcaption>Raspberry Pi Camera</figcaption>
+    </figure>
+  </li>
+  <li>
